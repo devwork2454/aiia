388ad9f49ef1468e2002b1995ef87207c012af4f
migoachi <migoachi@gmail.com>
2026-08-14 18:02:19 +0800

    chore: remove debug renderer

--- pi-agent/extensions/debug-renderer.js
+++
@@ -1,8 +0,0 @@
-export default function debugRenderer(pi) {
-  if (typeof pi.registerMessageRenderer === "function") {
-    pi.registerMessageRenderer("assistant", (msg) => {
-      require("fs").appendFileSync("/tmp/pi-debug.log", JSON.stringify({role: msg.role, customType: msg.customType}) + "\n");
-      return undefined;
-    });
-  }
-}
